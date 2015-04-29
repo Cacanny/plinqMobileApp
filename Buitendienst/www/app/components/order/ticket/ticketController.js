803d5c416ab1e24900fc2a3a16780ef89e17693e
Mike <mike.gitaar@hotmail.com>
2015-04-29 12:09:38 +0200

    Ticketnotitie hide/show weggehaald en gefixt

--- Buitendienst/www/app/components/order/ticket/ticketController.js
+++ Buitendienst/www/app/components/order/ticket/ticketController.js
@@ -2,11 +2,26 @@ angular.module('directory.ticketController', [])
 
     .controller('TicketCtrl', function ($scope, $filter) {   
         
-        // Initial state of ng-show for the Ticketnotities
-        $scope.showme = true;
-
-        $scope.toggleSlice = function (notitie) {
-            notitie = $filter('getSlice')(notitie);
-            alert(notitie);
+        // Determine if the tickettab should be showed or not
+    	$scope.showTickets = true;
+        
+        if (JSON.stringify($scope.order.ticket) === '{}') {
+            $scope.showTickets = false;
         }
+
+        // Toggle the filter for 'notitie' on click
+        var shouldRunFilter = false;
+        var originalNote;
+
+        $scope.toggleFilter = function (_notitie, index){
+        	shouldRunFilter = !shouldRunFilter;
+
+        	if(shouldRunFilter) {
+        		originalNote = $scope.order.ticket.ticketregels[index].notitie;
+        		$scope.order.ticket.ticketregels[index].notitie = $filter('getSlice')(_notitie);
+        	} else {
+        		$scope.order.ticket.ticketregels[index].notitie = originalNote;
+        	}
+    	}
+    
     });
