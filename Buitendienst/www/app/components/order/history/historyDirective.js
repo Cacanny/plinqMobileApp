58cd3de245137944b094badbae2ed5118d7f876c
Cacanny <dannyhoeve@gmail.com>
2015-05-14 12:09:55 +0200

    History View, Ctr, Filter en Directve

---
+++ Buitendienst/www/app/components/order/history/historyDirective.js
@@ -0,0 +1,9 @@
+﻿angular.module('directory.historyDirective', [])
+    .directive('historyView', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/components/order/history/historyView.html',
+            controller: 'HistoryCtrl'
+        }
+
+    });
